6fc672ee1cb5bf30ec3abccb4be860e92de6c546
Martin McClure <martin@hand2mouse.com>
2025-12-07 23:07:25 -0800

    Some of this is real debugging. Not sure about the big deletion

--- dali/driver/hasseb.py
+++ dali/driver/hasseb.py
@@ -33,7 +33,7 @@ HASSEB_DRIVER_SNIFFER_BYTE = 5
 HASSEB_DRIVER_SNIFFER_BYTE_ERROR = 6
 
 
-class HassebDALIUSBNoDataAvailable(object):
+class HassebDALIUSBNoDataAvailable:
     def __repr__(self):
         return 'NO DATA AVAILABLE'
 
@@ -80,13 +80,17 @@ class HassebDALIUSBDriver(DALIDriver):
 
     def __init__(self, path=None):
         try:
+            self.device = hid.device()
             if path:
-                self.device = hid.Device(path=path)
+                self.device.open_path(path)
             else:
-                self.device = hid.Devide(vid=HASSEB_USB_VENDOR, pid=HASSEB_USB_PRODUCT)
+                self.device.open(HASSEB_USB_VENDOR, HASSEB_USB_PRODUCT)
             self.device_found = 1
         except:
-            self.device_found = 'Exception happened'
+            import sys
+            logging.getLogger("hasseb.py").warning("device.open error: {}".format(sys.exception()))
+            self.device_found = None
+        logging.getLogger("hasseb.py").warning("device_found is: {}".format(self.device_found))
 
     def run_sequence(self, seq, progress_cb=None):
         from dali.gear.general import EnableDeviceType
@@ -283,209 +287,6 @@ def SyncHassebDALIUSBDriverFactory():
 
     hasseb_hid_devices = hid.enumerate(HASSEB_USB_VENDOR, HASSEB_USB_PRODUCT)
     for hasseb_hid_device in hasseb_hid_devices:
-        logging.getLogger("SyncHassebDALIUSBDriverFactory").debug("device found, path is {}".format(hasseb_hid_device.get('path', 'No path found')))
-        hasseb_dali_drivers.append(SyncHassebDALIUSBDriver(hasseb_hid_device.get('path')))
+        hasseb_dali_drivers.append(SyncHassebDALIUSBDriver(hasseb_hid_device['path']))
 
     return hasseb_dali_drivers
-        
-
-class Device(object):
-    """Any DALI slave device that has been configured with a short address."""
-
-    def __init__(self, address, bus=None, randomAddress=None, deviceType=None, groups=None):
-        if not isinstance(address, int) or address < 0 or address > 63:
-            raise ValueError("address must be an integer in the range 0..63")
-        self.address = address
-        self.address_obj = Short(address)
-        self.bus = None
-        if bus:
-            self.bind(bus)
-        self.randomAddress = randomAddress
-        self.deviceType = deviceType
-        self.groups = groups
-
-    def bind(self, bus):
-        """Bind this device object to a particular DALI bus."""
-        bus.add_device(self)
-
-
-class Bus(object):
-    """A DALI bus."""
-
-    _all_addresses = set(range(64))
-
-    def __init__(self, name=None, interface=None):
-        self._devices = {}
-        self._bus_scanned = False  # Have we scanned the bus for devices?
-        self.name = name
-        self._interface = interface
-
-    def add_device(self, device):
-        if device.bus and device.bus != self:
-            print("Device already bound")
-        if device.address in self._devices:
-            print("Duplicate device")
-        if not isinstance(device.address, int) or device.address < 0 \
-                or device.address > 63:
-            print("Device address is invalid")
-        self._devices[device.address] = device
-        device.bus = self
-
-    def unused_addresses(self):
-        """Return all short addresses that are not in use."""
-        used_addresses = set(self._devices.keys())
-        return list(self._all_addresses - used_addresses)
-
-    def scan(self):
-        """Scan the bus for devices and ensure there are device objects for
-        each discovered device.
-        """
-        i = self.get_interface()
-        for sa in range(64):
-            if sa in self._devices:
-                continue
-            response = i.send(QueryControlGearPresent(address.Short(sa)))
-            if response.value:
-                Device(address=sa, bus=self)
-        self._bus_scanned = True
-
-    def set_search_addr(self, addr):
-        i = self.get_interface()
-        i.send(SetSearchAddrH((addr >> 16) & 0xff))
-        i.send(SetSearchAddrM((addr >> 8) & 0xff))
-        i.send(SetSearchAddrL(addr & 0xff))
-
-    def find_next(self, low, high):
-        """Find the ballast with the lowest random address.  The caller
-        guarantees that there are no ballasts with an address lower
-        than 'low'.
-
-        If found, returns the random address.  SearchAddr will be set
-        to this address in all ballasts.  The ballast is not
-        withdrawn.
-
-        If not found, returns None.
-        """
-        i = self.get_interface()
-        self.set_search_addr(high)
-        if low == high:
-            response = i.send(gear.Compare())
-            if response.value is True:
-                return low
-            return None
-        response = i.send(Compare())
-        if response.value is True:
-            midpoint = (low + high) // 2
-            return self.find_next(low, midpoint) \
-                or self.find_next(midpoint + 1, high)
-
-    def search_bus(self, broadcast=False):
-        """ Initialize bus with broadcast on or off and find the devices from the bus
-
-        """
-        addrs = self.unused_addresses()
-        i = self.get_interface()
-        i.send(Terminate())
-        i.send(Initialise(broadcast=broadcast, address=None))
-        i.send(Randomise())
-        # Randomise may take up to 100ms
-        time.sleep(0.1)
-        low = 0
-        high = 0xffffff
-        while low is not None:
-            low = self.find_next(low, high)
-            if low is not None:
-                if addrs:
-                    new_addr = addrs.pop(0)
-                    i.send(ProgramShortAddress(new_addr))
-                    r = i.send(VerifyShortAddress(new_addr))
-                    if r.value is not True:
-                        print(f"Error in programming short address {new_addr}")
-                    i.send(gear.Withdraw())
-                    Device(address=new_addr, randomAddress=low, bus=self)
-                else:
-                    i.send(Terminate())
-                    print("No free address")
-                low = low + 1
-        i.send(Terminate())
-
-    def assign_short_addresses(self):
-        """Search for devices on the bus with no short address allocated, and
-        allocate each one a short address from the set of unused
-        addresses.
-        """
-        if not self._bus_scanned:
-            self.scan()
-        self.search_bus(broadcast=False)
-        self.query_device_types()
-        self.query_groups()
-
-    def initialize_bus(self):
-        """Initialize bus
-        """
-        self._devices = {}
-        self.search_bus(broadcast=True)
-        self.query_device_types()
-        self.query_groups()
-
-    def query_device_types(self):
-        """Find the device types of the devices in the bus
-        """
-        i = self.get_interface()
-        for sa in range(64):
-            if sa in self._devices:
-                self._devices[sa].deviceType = i.send(gear.QueryDeviceType(sa))
-
-    def query_groups(self):
-        """Find the groups of the devices in the bus
-        """
-        i = self.get_interface()
-        for sa in range(64):
-            if sa in self._devices:
-                group1 = i.send(gear.QueryGroupsZeroToSeven(sa))
-                group2 = i.send(gear.QueryGroupsEightToFifteen(sa))
-                try:
-                    group1 = group1.value.as_integer
-                    group2 = group2.value.as_integer
-                except:
-                    self._devices[sa].group = None
-                else:
-                    self._devices[sa].groups = self.parse_groups(group1, group2)
-
-    def parse_groups(self, group1, group2):
-        groups = ""
-        if (group1 & 1<<0) != 0:
-            groups += "0, "
-        if (group1 & 1<<1) != 0:
-            groups += "1, "
-        if (group1 & 1<<2) != 0:
-            groups += "2, "
-        if (group1 & 1<<3) != 0:
-            groups += "3, "
-        if (group1 & 1<<4) != 0:
-            groups += "4, "
-        if(group1 & 1 << 5) != 0:
-            groups += "5, "
-        if (group1 & 1 << 6) != 0:
-            groups += "6, "
-        if (group1 & 1 << 7) != 0:
-            groups += "7, "
-        if (group2 & 1 << 0) != 0:
-            groups += "8, "
-        if (group2 & 1 << 1) != 0:
-            groups += "9, "
-        if (group2 & 1 << 2) != 0:
-            groups += "10, "
-        if (group2 & 1 << 3) != 0:
-            groups += "11, "
-        if (group2 & 1 << 4) != 0:
-            groups += "12, "
-        if (group2 & 1 << 5) != 0:
-            groups += "13, "
-        if (group2 & 1 << 6) != 0:
-            groups += "14, "
-        if (group2 & 1 << 7) != 0:
-            groups += "15, "
-        groups = groups[:-2]
-
-        return groups
